3352c320f186a700102fffbfb35c727214937006
helloitsjoe <jboyle617@gmail.com>
2021-07-20 13:58:44 -0400

    Some basic neighbor checking

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -16,7 +16,72 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(s: &str) {
-    let together = format!("{}, {}", "Hello", s);
-    log(&together);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead= 0,
+    Alive= 1,
+}
+
+pub struct Universe {
+    width: u32,
+    _height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    pub fn new(width: u32, _height: u32) -> Universe {
+        Universe {
+            width,
+            _height,
+            cells: vec![Cell::Dead; (width * _height) as usize],
+        }
+    }
+
+    pub fn set_live(&mut self, col: u32, row: u32) {
+        let idx =self.get_index(col, row);
+        self.cells[idx] = Cell::Alive;
+    }
+
+    pub fn get_live(&self, col: u32, row: u32) -> Cell {
+        self.cells[self.get_index(col, row)]
+    }
+
+    fn get_index(&self, col: u32, row: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn check_index(&self, idx: i32) -> bool {
+        if idx < 0 {
+            return false
+        }
+        self.cells.get(idx as usize) == Some(&Cell::Alive)
+    }
+
+
+    pub fn live_neighbor_count(&self, col: u32, row: u32) -> u32 {
+        let home_idx = self.get_index(col, row) as i32;
+        let mut count: u32 = 0;
+
+        // log(&format!("{:?}", self.cells));
+
+        let neighbors = vec![
+            home_idx - 1,
+            home_idx + 1,
+            home_idx + self.width as i32,
+            home_idx - self.width as i32,
+            home_idx - 1 - (self.width) as i32,
+            home_idx + 1 - (self.width) as i32,
+            home_idx - 1 + (self.width) as i32,
+            home_idx + 1 + (self.width) as i32,
+        ];
+
+        for idx in neighbors {
+            if self.check_index(idx) {
+                count += 1;
+            }
+        }
+
+        count
+    }
 }
--- crate/tests/web.rs
+++ crate/tests/web.rs
@@ -2,12 +2,52 @@
 
 #![cfg(target_arch = "wasm32")]
 
+extern crate wasm_game_of_life;
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+use wasm_game_of_life::{Universe, Cell};
 
-wasm_bindgen_test_configure!(run_in_browser);
+// wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn set_live() {
+  let mut u = Universe::new(5, 6);
+  assert_eq!(u.get_live(0, 0), Cell::Dead);
+  u.set_live(0, 0);
+  assert_eq!(u.get_live(0, 0), Cell::Alive);
+}
+
+#[wasm_bindgen_test]
+fn count_neighbors() {
+  let mut u = Universe::new(5, 6);
+  u.set_live(2, 2);
+  u.set_live(3, 2);
+  assert_eq!(u.live_neighbor_count(2, 2), 1);
+  u.set_live(1, 2);
+  assert_eq!(u.live_neighbor_count(2, 2), 2);
+  u.set_live(2, 1);
+  u.set_live(2, 3);
+  assert_eq!(u.live_neighbor_count(2, 2), 4);
+  u.set_live(1, 1);
+  u.set_live(3, 1);
+  u.set_live(3, 3);
+  u.set_live(1, 3);
+  assert_eq!(u.live_neighbor_count(2, 2), 8);
+}
+
+#[wasm_bindgen_test]
+fn count_neighbors_edge() {
+  let mut u = Universe::new(5, 5);
+  assert_eq!(u.live_neighbor_count(0, 0), 0);
+  u.set_live(1, 0);
+  u.set_live(0, 1);
+  u.set_live(1, 1);
+  assert_eq!(u.live_neighbor_count(0, 0), 3);
+  // Lower right
+  assert_eq!(u.live_neighbor_count(4, 4), 0);
+  // Surround lower right
+  u.set_live(4, 3);
+  u.set_live(3, 4);
+  u.set_live(3, 3);
+  assert_eq!(u.live_neighbor_count(4, 4), 3);
 }
